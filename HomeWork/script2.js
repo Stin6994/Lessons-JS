7bf1069c67ea48fa6a87d38e5d5e3386b4f4d8d7
Stin9469 <Stinger1994@mail.ru>
2024-06-25 11:48:12 +0300

    Упражнение 21 - Задание на работу с методами массивов (выполнено успешно)

--- HomeWork/script2.js
+++ HomeWork/script2.js
@@ -1,5 +1,7 @@
 console.log('test2');
 
+// Упражнение 21 - Задание на работу с методами массивов
+
 /* Задачи:
 
 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. 
@@ -24,10 +26,23 @@ const funds = [
     {amount: -11400}
 ];
 
-const getPositiveIncomeAmount = (data) => {
 
+const getPositiveIncomeAmount = (data) => {
+    return data.filter(item => item.amount >=0)
+    .map(item => item.amount)
+    .reduce((sum, current) => sum + current);
 };
+/* console.log(getPositiveIncomeAmount(funds)); */
+
+
+/* const arr = funds.map(item => item.amount)
+.reduce((sum, current) => (funds.some(item => item.amount < 0)) ? sum + current : `dfsd` );
+console.log(arr); */
+
 
 const getTotalIncomeAmount = (data) => {
-
+    return data.map(item => item.amount)
+    .reduce((sum, current) => (funds.some(item => item.amount < 0)) ? sum + current : getPositiveIncomeAmount(data));
 };
+
+/* console.log(getTotalIncomeAmount(funds)); */
