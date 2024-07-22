5ab8b4220235c5744e4bae150663fcb5d7a2fd43
Stin9469 <Stinger1994@mail.ru>
2024-07-22 11:58:56 +0300

    Урок 93 - навигация слайдера создана

--- Food_Study_Project/js/script.js
+++ Food_Study_Project/js/script.js
@@ -540,6 +540,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Урок 92 - слайдер, второй вариант (карусель)
 
     const slides = document.querySelectorAll('.offer__slide'),
+        slider = document.querySelector('.offer__slider'),
         prev = document.querySelector('.offer__slider-prev'),
         next = document.querySelector('.offer__slider-next'),
         total = document.querySelector('#total'),
@@ -572,6 +573,69 @@ document.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    slider.style.position = 'relative';
+
+    const indicators = document.createElement('ol'); //создаем список(ol - order-list) из точек навигации
+
+    const dots = []; //создаем массив для точек
+
+    indicators.classList.add('carousel-indicators'); // даем ему класс
+    indicators.style.cssText = ` 
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `; //классу присваиваем css свойства, можно просто сделать это в css
+
+    slider.append(indicators); //помещаем индикаторы внутрь блока слайдера
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li'); //создаем точки по количеству слайдов (li - list-item)
+        dot.setAttribute('data-slide-to', i + 1); // каждому слайду, начиная с 1 задаем аттрибут
+        dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+        `;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        } 
+
+        indicators.append(dot); //добавляем точку в верстку
+        dots.push(dot); // добавляем точку в массив
+    }
+
+
+    function slidesSetting() {
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+        dots.forEach(dot => dot.style.opacity = '.5'); // для изменения активного индикатора
+        dots[slideIndex-1].style.opacity = 1;
+    }
+ 
+
+
     next.addEventListener('click', () => {
         if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) { //если текущее положение индекса конечное, то возвращаемся к начальному
             // +width.slice(0,width.length - 2) - изначально строка, например "500px". Обрезаем рх, преобразуем в число
@@ -587,11 +651,7 @@ document.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        slidesSetting();
 
     });
 
@@ -608,15 +668,21 @@ document.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        if (slides.length < 10) {
-            current.textContent = `0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        slidesSetting();
     });
 
     // Урок 93 - навигация для слайдера
 
+    dots.forEach(dot => {   // для перемещения по слайдам кликом по навигации
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+            slideIndex = slideTo; // приравниваем индекс к выбранному слайду, чтобы все правила завязанные на индекс переформатировались под выбранный слайд
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1); //находим положение нужного слайда
+            slidesField.style.transform = `translateX(-${offset}px)`; //перемещаем на нужный слайд
+            
+            slidesSetting();
+        });
+    });
 
 
 });
--- Food_Study_Project/setPATH.bat
+++ Food_Study_Project/setPATH.bat
@@ -1,3 +1,3 @@
-@cd Z:\UniServerZ\www\Lessons-JS-main\Lessons-JS\Food Study Project
+@cd Z:\UniServerZ\www\Lessons-JS-main\Lessons-JS\Food_Study_Project
 @set PATH=Z:\node-v20.15.0-win-x64;%PATH%
 @cmd.exe /K
