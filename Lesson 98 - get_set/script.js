eed8814870cf5c1782efeb2981e8a4fc5138382e
Stin9469 <Stinger1994@mail.ru>
2024-07-23 14:35:21 +0300

    Урок 98 - Акцессоры (get/set) - конспект

--- Lesson 98 - get_set/script.js
+++ Lesson 98 - get_set/script.js
@@ -2,3 +2,23 @@
 
 // Урок 98 - Акцессоры (get/set)
 
+const persone = {
+    name: 'Alex',
+    age: 26,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    }
+
+}
+
+console.log(persone.userAge); //из-зи get можно использовать функцию без ()
+console.log(persone.userAge = 30); // set позволил взять число извне и изменить свойство объекта
+console.log(persone.userAge);
+
+// Урок 99 - Инкапсуляция (ООП)
+
