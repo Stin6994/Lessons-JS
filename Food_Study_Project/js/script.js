3fb60a1e9d11e1c7ecfe54c726715c82b575c550
Stin9469 <Stinger1994@mail.ru>
2024-07-23 12:18:28 +0300

    Уроки 96-97 готов алгоритм расчета, баг при нажатии мимо выбираемых элементов

--- Food_Study_Project/js/script.js
+++ Food_Study_Project/js/script.js
@@ -692,6 +692,79 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Уроки 96-97 Калькулятор
 
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    function calcTotal() { //функция для расчета
+        if (!sex || !height || !weight || !age || !ratio) { // если что-то не заполнено, то расчитывать не будем
+            result.textContent = 'WTF';
+            return;
+        }
+
+        if (sex === 'female') { // если для женщины, формула одна, для мужчины другая
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parrentSelector, activeClass) { //функция для сбора данных, из незаполняемых (выбранных) элементов
+        const elements = document.querySelectorAll(`${parrentSelector} div`); //получаем все div из элемента parrentSelector
+        document.querySelector(parrentSelector).addEventListener('click', (e) => { // при нажатии на один из елементов
+            if (e.target.getAttribute('data-ratio')) { // проверяем есть ли у него аттрибут data-ratio
+                // такие аттрибуты заранее прописали для степеней активности, так мы отделяем
+                // было ли нажатие на элемент пола или элемент активности
+                ratio = +e.target.getAttribute('data-ratio'); // достаем значение аттрибута, преобразуем в число, присваеваем одноименной переменной
+            } else { // если это не элемент активности, то элемент пола, а у них вместо data-ratio мы прописали id для их разделения
+                sex = e.target.getAttribute('id');
+            }
+
+            console.log(ratio, sex);
+
+            elements.forEach(elem => {
+                elem.classList.remove(activeClass); //для выделения только выбранного блока для начала убираем выделение со всех
+            });
+
+            e.target.classList.add(activeClass); //и выделяем только выбранный блок
+
+            calcTotal(); // пересчитываем после любого изменения
+        });
+    }
+
+    // вызываем функцию 2 раза, первая на блок пола, вторая на активность. Эти блоки одинаковы по функционалу, поэтому 
+    // функция одна, только идентификаторы разные
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+
+    function getDynamicInformation(selector) { // функция для сбора данных, из заполняемых полей
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            //надо понять в какое поле вводится инфа - при помощи id полей
+            
+            switch(input.getAttribute('id')) { // когда в одно из полей будем что-то вводить, код будет понимать в какое поле вводим по id
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;    
+            }
+            calcTotal(); // пересчитываем после любого изменения
+        });
+
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 
 });
 
