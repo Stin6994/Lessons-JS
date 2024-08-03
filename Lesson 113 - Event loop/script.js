cd98295a8507b155f1b5f199d9ebfbca52acdd51
Stin6994 <Stinger1994@mail.ru>
2024-08-03 21:57:55 +0300

    Урок 113 - Event loop - конспект

--- Lesson 113 - Event loop/script.js
+++ Lesson 113 - Event loop/script.js
@@ -2,7 +2,7 @@
 
 // Урок 113 - Event loop
 
-console.log(1);
+/* console.log(1);
 
 setTimeout(() => {
     console.log('timeout');
@@ -13,6 +13,25 @@ setTimeout(() => {
 }, 4000);
 
 console.log(21);
-
+ */
 // Очередность вывода - 1, 21, timeout, timeout2
 
+/* let k = 0;
+
+function count() {
+    for (let i = 0; i < 1e9; i++) {
+        k++;
+    }
+    alert ('done');
+}
+
+count(); */// эта функция перегружает браузер, пока не выполнится - даже div с текстом не отобразится
+
+setTimeout(() => {
+    console.log('1');
+}, 0);
+
+console.log(2);
+
+//сначала выполнится 2, потом 1, так как setTimeout - асинхронная операция
+//и несмотря на 0 секунд выполнения - она пойдет в работу после синхронной console.log
