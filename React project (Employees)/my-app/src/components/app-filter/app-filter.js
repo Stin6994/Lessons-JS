599ef6fafe017a9b38e3df750fbbc32aa84c6d95
Stin6994 <Stinger1994@mail.ru>
2024-08-12 18:45:23 +0300

    Урок 125 - Создание нового проекта - верстка шаблона окончена

---
+++ React project (Employees)/my-app/src/components/app-filter/app-filter.js
@@ -0,0 +1,26 @@
+import './app-filter.css';
+
+const AppFilter = () => {
+    return (
+        <div className="btn-group">  {/* bootstrap класс */}
+
+            <button className="btn btn-light"
+            type="button">
+                Все сотрудники
+            </button>
+
+            <button className="btn btn-outline-light"
+            type="button">
+                На повышение
+            </button>
+
+            <button className="btn btn-outline-light"
+            type="button">
+                З/П выше 120 000 руб.
+            </button>
+
+        </div>
+    );
+}
+
+export default AppFilter;
