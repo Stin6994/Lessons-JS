ca557d39587e9ecf52dcefb3936c82dca088812a
Stin6994 <Stinger1994@mail.ru>
2024-08-16 10:39:00 +0300

    Урок 135 - печенька отрабатывает

--- React project (Employees)/my-app/src/components/app/app.js
+++ React project (Employees)/my-app/src/components/app/app.js
@@ -24,7 +24,7 @@ class App extends Component {
 
     deleteItem = (id) => {
         this.setState(({data}) =>{ //чтобы удалить нужную строку, надо понять на строку с каким индексом мы нажали
-            /* const index = data.findIndex(elem => elem.id == id); *///при нажатии будет перезаписывать 
+            /* const index = data.findIndex(elem => elem.id === id); *///при нажатии будет перезаписывать 
             //id строки из заданной в свойствах на порядковый номер в массиве (0,1,2...)
             
             //сейчас делаем удаление на основе принципов иммутабельности (нельзя изменять объект с данными в состоянии)
@@ -43,6 +43,7 @@ class App extends Component {
             name, 
             salary,
             increase: false,
+            rise: false,
             id: this.maxId++
         }
         this.setState(({data}) => {
@@ -54,11 +55,41 @@ class App extends Component {
     }
    
 onToggleIncrease = (id) => {
-    console.log(`Increase this ${id}`); //методы, которые будем передавать вниз по иерархии
+    /* console.log(`Increase this ${id}`); */ //методы, которые будем передавать вниз по иерархии
+    /* this.setState(({data}) => { */
+            //первый способ
+        /* const index = data.findIndex(elem => elem.id === id); //получаем индекс элемента, с которым будем работать
+        const old = data[index]; //получили объект из data по индексу
+        const newItem = {...old, increase: !old.increase};
+            // создали новый объект (соблюдаем таким образом принцип иммутабельности)
+            //конструкция {...old} - повторяет в новом оюъекте все свойста старого
+            // после запятой - мы либо добавляем новые свойства, либо, если свойство повторяется, то оно перезаписывает одноименное
+            //мы перезаписываем свойство increase на противоположное при клике
+        console.log(newItem);
+        const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+        console.log(newArr);
+        return {
+            data: newArr
+        } */
+            //второй способ
+
+            //что происходит:
+            //1) так как data - массив, можем исподьзовать map - вернет новый переработанный массив (иммутабельность)
+            //2) мы перебираем все объекты внутри (item), пока не наткнемся на элемент с таким же id, на который кликнули
+            //3) метод map переработает только этот объект, благодаря условию if (все свойства останутся, inrease поменяется)
+            //4) По итогу map вернет новый массив в состояние, в нем поменяется только increase у того компонента, на который кликнули
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, increase: !item.increase}
+                }
+                return item;
+            })
+        }))
 }
 
 onToggleRise = (id) => {
-    console.log(`Rise this ${id}`); //методы, которые будем передавать вниз по иерархии
+    /* console.log(`Rise this ${id}`); */ //методы, которые будем передавать вниз по иерархии
 }
 
     render() {
