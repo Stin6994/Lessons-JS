18f2c220dafa87055c67b7cbc2c70ef42525b21f
Stin6994 <Stinger1994@mail.ru>
2024-08-17 11:06:58 +0300

    Урок 141 - Динамические стили и классы

--- React project (Employees)/my-app/src/components/app-filter/app-filter.js
+++ React project (Employees)/my-app/src/components/app-filter/app-filter.js
@@ -3,20 +3,22 @@ import './app-filter.css';
 const AppFilter = (props) => {
 
     const buttonsData = [
-        { name: 'all', label: 'Все сотрудники' },
-        { name: 'rise', label: 'На повышение' },
-        { name: 'moreThen 120000', label: 'З/П выше 120 000 руб.' }
+        { name: 'all', label: 'Все сотрудники', colored: false },
+        { name: 'rise', label: 'На повышение', colored: false },
+        { name: 'moreThen 120000', label: 'З/П выше 120 000 руб.', colored: true }
     ];
 
-    const buttons = buttonsData.map(({ name, label }) => {
+    const buttons = buttonsData.map(({ name, label, colored }) => {
         const active = props.filter === name; // active будет true, если заданный фильтр будет равен какому-то из name кнопок
         const clazz = active ? 'btn-light' : 'btn-outline-light' // если active true, то один класс и наоборот
+        const style = colored ? {color: 'red'}: null; //если true, то добавляем красный цвет, если false - ничего не делаем
         return (
             <button 
                 className={`btn ${clazz}`} // в зависимости от выбранного фильтра будет либо активной либо нет
                 type="button"
                 key={name}
-                onClick={() => props.onFilterSelect(name)}> 
+                onClick={() => props.onFilterSelect(name)}
+                style={style}> 
                 {label}
             </button>
         )
