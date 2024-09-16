8da544717de8cbfddc8b878340da16f2899080d1
Stin9469 <Stinger1994@mail.ru>
2024-09-16 16:48:43 +0300

    Урок 152 - Предохранители (Error Boundaries) - начало

--- React project (Marvel)/my-app/src/components/charInfo/CharInfo.js
+++ React project (Marvel)/my-app/src/components/charInfo/CharInfo.js
@@ -42,6 +42,8 @@ class CharInfo extends Component {
             .getCharacter(charId)
             .then(this.onCharLoaded)
             .catch(this.onError);
+
+        this.foo.bar = 0; //несуществующее свойство
     }
 
 
