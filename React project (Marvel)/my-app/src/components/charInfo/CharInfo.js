a39511b1afef0af96bce8c28fd5ad943cee62d87
Stin9469 <Stinger1994@mail.ru>
2024-09-16 16:46:55 +0300

    Урок 151 - фрагмент с комиксами работает - конец урока

--- React project (Marvel)/my-app/src/components/charInfo/CharInfo.js
+++ React project (Marvel)/my-app/src/components/charInfo/CharInfo.js
@@ -67,19 +67,19 @@ class CharInfo extends Component {
 
 
     render() {
-        const {char, loading, error} = this.state
+        const { char, loading, error } = this.state
 
-        const skeleton = char || loading || error ? null : <Skeleton/>
-        const errorMessage = error ? <ErrorMassage /> : null; 
-        const spinner = loading ? <Spinner /> : null; 
-        const content = !(loading || error || !char) ? <View char={char} /> : null; 
+        const skeleton = char || loading || error ? null : <Skeleton />
+        const errorMessage = error ? <ErrorMassage /> : null;
+        const spinner = loading ? <Spinner /> : null;
+        const content = !(loading || error || !char) ? <View char={char} /> : null;
 
         return (
             <div className="char__info">
-               {skeleton}
-               {errorMessage}
-               {spinner}
-               {content}
+                {skeleton}
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
         )
     }
@@ -87,45 +87,51 @@ class CharInfo extends Component {
 }
 
 
-const View = ({char}) => {
+const View = ({ char }) => {
 
     const { name, description, thumbnail, homepage, wiki, comics } = char
+    const imgNotFound = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
 
     return (
         <>
-        <div className="char__basics">
-        <img src={thumbnail} alt={name} />
-        <div>
-            <div className="char__info-name">{name}</div>
-            <div className="char__btns">
-                <a href={homepage} className="button button__main">
-                    <div className="inner">Homepage</div>
-                </a>
-                <a href={wiki} className="button button__secondary">
-                    <div className="inner">Wiki</div>
-                </a>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} style={thumbnail === imgNotFound ? { objectFit: 'contain' } : { objectFit: 'cover' }}/>
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">Homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div className="char__descr">
-        {description}
-    </div>
-    <div className="char__comics">Comics:</div>
-    <ul className="char__comics-list">
-       
-        {
-            comics.map((item, i) => {
-                return (
-                    <li className="char__comics-item"
-                    key={i}>
-                    {item.name}
-                </li>
-                )
-            })
-        }
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
 
-    </ul>
-    </>
+                {comics.length > 0 ? null : 'There is no comics with this character'}
+
+                {
+                    comics.map((item, i) => {
+
+                        if (i > 9) return;  //по макету - не больше 10 комиксов подгружается
+
+                        return (
+                            <li className="char__comics-item"
+                                key={i}>
+                                {item.name}
+                            </li>
+                        )
+                    })
+                }
+
+            </ul>
+        </>
     )
 }
 
