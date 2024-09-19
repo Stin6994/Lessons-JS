7f9095d469169b98d8a6717f08d5e599f2e8f0a9
Stin9469 <Stinger1994@mail.ru>
2024-09-19 14:34:49 +0300

    Урок 158 - ref дополнен. Фокус на выбираемых персонажах, возможность выбирать их при помощи клавиатуры

--- React project (Marvel)/my-app/src/components/charList/CharList.js
+++ React project (Marvel)/my-app/src/components/charList/CharList.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-
 import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
@@ -14,18 +13,18 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1552,
+        offset: 1549,
         charEnded: false
     }
 
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.onCharListLoading();
         this.onRequest();
     }
 
     onRequest = (offset) => {
+        this.onCharListLoading();
         this.marvelService
             .getAllCharacters(offset)
             .then(this.onCharListLoaded)
@@ -42,15 +41,15 @@ class CharList extends Component {
     onCharListLoaded = (newCharList) => {
         console.log('update');
         let ended = false;
-        if (newCharList.length < 9) {
+        if (newCharList.length < 6) {
             ended = true;
         }
 
-        this.setState(({offset, charList}) =>({ // offset, charList - те свойства, которые зависят от предыдущих состояний, поэтому деструктурируем тут
+        this.setState(({ offset, charList }) => ({ // offset, charList - те свойства, которые зависят от предыдущих состояний, поэтому деструктурируем тут
             charList: [...charList, ...newCharList],  // при первом запуске отрисовки будет только 9 элементов, при втором старые элементы + еще 9 (по кнопке)
             loading: false,
             newItemLoading: false,
-            offset: offset + 9,
+            offset: offset + 6,
             charEnded: ended
         }))
         console.log('LOAD')
@@ -64,15 +63,39 @@ class CharList extends Component {
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
     view = (arr) => {
 
         const imgNotFound = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
-        const cardCharList = arr.map((item) => {
+        const cardCharList = arr.map((item, i) => {
 
             return (
                 <li className="char__item"
+                    tabIndex={0}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    ref={this.setRef}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i)
+                    }}
+                    onKeyPress={(e) => {
+                        e.preventDefault()
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                     <img src={item.thumbnail} alt={item.name}
                         style={item.thumbnail === imgNotFound ? { objectFit: 'unset' } : { objectFit: 'cover' }} />
                     <div className="char__name" style={{ fontSize: '16px' }}> {item.name}</div>
@@ -112,7 +135,7 @@ class CharList extends Component {
                 <button className="button button__main button__long"
                     disabled={newItemLoading}
                     onClick={() => this.onRequest(offset)}
-                    style={{'display' : charEnded ? 'none' : 'block'}}>
+                    style={{ 'display': charEnded ? 'none' : 'block' }}>
                     <div className="inner">load more</div>
                 </button>
             </div>
--- React project (Marvel)/my-app/src/services/MarvelService.js
+++ React project (Marvel)/my-app/src/services/MarvelService.js
@@ -20,7 +20,7 @@ class MarvelService {
     }
 
     getAllCharacters = async (offset = this._baseOffset) => { //получаем 9 персонажей, начиная с 356 позиции
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=6&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
 
