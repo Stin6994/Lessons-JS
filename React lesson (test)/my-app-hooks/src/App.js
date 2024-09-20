47525c90f0bd07a428ba7e8e043a31a3d5c6c6a0
Stin9469 <Stinger1994@mail.ru>
2024-09-20 17:29:02 +0300

    Урок 162 - useState - конспект

--- React lesson (test)/my-app-hooks/src/App.js
+++ React lesson (test)/my-app-hooks/src/App.js
@@ -1,7 +1,8 @@
-import {Component} from 'react';
-import {Container} from 'react-bootstrap';
+import { Component, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
-class Slider extends Component {
+
+/* class Slider extends Component {
 
     constructor(props) {
         super(props);
@@ -12,13 +13,13 @@ class Slider extends Component {
     }
 
     changeSlide = (i) => {
-        this.setState(({slide}) => ({
+        this.setState(({ slide }) => ({
             slide: slide + i
         }))
     }
 
     toggleAutoplay = () => {
-        this.setState(({autoplay}) => ({
+        this.setState(({ autoplay }) => ({
             autoplay: !autoplay
         }))
     }
@@ -28,15 +29,15 @@ class Slider extends Component {
             <Container>
                 <div className="slider w-50 m-auto">
                     <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                    <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
+                    <div className="text-center mt-5">Active slide {this.state.slide} <br /> {this.state.autoplay ? 'auto' : null}</div>
                     <div className="buttons mt-3">
-                        <button 
+                        <button
                             className="btn btn-primary me-2"
                             onClick={() => this.changeSlide(-1)}>-1</button>
-                        <button 
+                        <button
                             className="btn btn-primary me-2"
                             onClick={() => this.changeSlide(1)}>+1</button>
-                        <button 
+                        <button
                             className="btn btn-primary me-2"
                             onClick={this.toggleAutoplay}>toggle autoplay</button>
                     </div>
@@ -44,36 +45,69 @@ class Slider extends Component {
             </Container>
         )
     }
+} */
+
+const calcValue = () => {
+    console.log('random');
+
+    return Math.random()* (50-1) + 1;
 }
 
 
-// const Slider = (props) => {
-//     return (
-//         <Container>
-//             <div className="slider w-50 m-auto">
-//                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-//                 <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
-//                 <div className="buttons mt-3">
-//                     <button 
-//                         className="btn btn-primary me-2"
-//                         onClick={() => this.changeSlide(-1)}>-1</button>
-//                     <button 
-//                         className="btn btn-primary me-2"
-//                         onClick={() => this.changeSlide(1)}>+1</button>
-//                     <button 
-//                         className="btn btn-primary me-2"
-//                         onClick={this.toggleAutoplay}>toggle autoplay</button>
-//                 </div>
-//             </div>
-//         </Container>
-//     )
-// }
+const Slider = (props) => {
+
+    /*  const slideStateArray = useState(); //возвращается массив из 2 элементов - 1)текущее состояние  2)функция - действие с состоянием
+     console.log(slideStateArray); */
+    const [slide, setSlide] = useState(() =>calcValue()); // 0 - начальное значение состояния
+    // таких состояний можно создать сколько
+    const [autoplay, setAutoplay] = useState(false);
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+        setSlide(slide => slide + i);
+    }
+
+    function toggleAutoplay() {
+        setAutoplay(!autoplay)
+    }
+
+/*         const [state, setState] = useState({slide: 0, autoplay: false});
+
+        function changeSlide(i) {
+            setState(state => ({...state, slide: state.slide + i}));
+        }
+    
+        function toggleAutoplay() {
+            setState(state => ({...state, autoplay: !state.autoplay}));
+        } */
+
+    return (
+        <Container>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br /> {autoplay ? 'auto' : null} </div>
+
+                <div className="buttons mt-3">
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-102)}>-1</button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(102)}>+1</button>
+                    <button
+                        className="btn btn-primary me-2"
+                        onClick={toggleAutoplay}>toggle autoplay</button>
+                </div>
+            </div>
+        </Container>
+    )
+}
 
 
 function App() {
-  return (
-        <Slider/>
-  );
+    return (
+        <Slider />
+    );
 }
 
 export default App;
