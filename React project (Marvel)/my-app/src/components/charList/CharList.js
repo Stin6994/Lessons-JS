356aa9bb39bb59fbe07fa25c7be1628fcecfcfde
Stin9469 <Stinger1994@mail.ru>
2024-09-24 15:08:29 +0300

    Урок 167 - переписывание проекта на хуки. Переписан CharList.js

--- React project (Marvel)/my-app/src/components/charList/CharList.js
+++ React project (Marvel)/my-app/src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
@@ -7,75 +7,64 @@ import ErrorMassage from '../errorMessage/ErrorMessage';
 
 import './charList.scss';
 
-class CharList extends Component {
-    state = {
-        charList: [],
-        loading: true,
-        error: false,
-        newItemLoading: false,
-        offset: 1549,
-        charEnded: false
-    }
+const CharList = (props) => {
 
-    marvelService = new MarvelService();
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(1549);
+    const [charEnded, setCharEnded] = useState(false);
 
-    componentDidMount() {
-        this.onRequest();
-    }
+    const marvelService = new MarvelService();
 
-    onRequest = (offset) => {
-        this.onCharListLoading();
-        this.marvelService
+    useEffect(() => {
+        onRequest();
+    }, []) //пустой массив - функция аналагичная componentDidMount запустится 1 раз в начале после рендеринга
+
+
+    const onRequest = (offset) => {
+        onCharListLoading();
+        marvelService
             .getAllCharacters(offset)
-            .then(this.onCharListLoaded)
-            .catch(this.onError)
+            .then(onCharListLoaded)
+            .catch(onError)
     }
 
-    onCharListLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
+    const onCharListLoading = () => {
+        setNewItemLoading(true);
     }
 
 
-    onCharListLoaded = (newCharList) => {
+    const onCharListLoaded = (newCharList) => {
         console.log('update');
         let ended = false;
         if (newCharList.length < 6) {
             ended = true;
         }
 
-        this.setState(({ offset, charList }) => ({ // offset, charList - те свойства, которые зависят от предыдущих состояний, поэтому деструктурируем тут
-            charList: [...charList, ...newCharList],  // при первом запуске отрисовки будет только 9 элементов, при втором старые элементы + еще 9 (по кнопке)
-            loading: false,
-            newItemLoading: false,
-            offset: offset + 6,
-            charEnded: ended
-        }))
-        console.log('LOAD')
-        /* console.log(charList) */
+        setCharList(charList => [...charList, ...newCharList]);
+        setLoading(false); // нам неважно какой раньше был loading, мы в любом случае ставим его в false на этом этапе, поэтому можеи передать только аргумент false
+        setNewItemLoading(newItemLoading => false);
+        setOffset(offset => offset + 6);
+        setCharEnded(charEnded => ended);
+
     }
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        })
+    const onError = () => {
+        setLoading(false);
+        setError(true);
     }
 
-    itemRefs = [];
+    const itemRefs = useRef([]);
 
-    setRef = (ref) => {
-        this.itemRefs.push(ref);
+    const focusOnItem = (id) => {
+        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+        itemRefs.current[id].classList.add('char__item_selected');
+        itemRefs.current[id].focus();
     }
 
-    focusOnItem = (id) => {
-        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-        this.itemRefs[id].classList.add('char__item_selected');
-        this.itemRefs[id].focus();
-    }
-
-    view = (arr) => {
+    const view = (arr) => {
 
         const imgNotFound = 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
         const cardCharList = arr.map((item, i) => {
@@ -84,16 +73,16 @@ class CharList extends Component {
                 <li className="char__item"
                     tabIndex={0}
                     key={item.id}
-                    ref={this.setRef}
+                    ref={el => itemRefs.current[i] = el}
                     onClick={() => {
-                        this.props.onCharSelected(item.id);
-                        this.focusOnItem(i)
+                        props.onCharSelected(item.id);
+                        focusOnItem(i)
                     }}
                     onKeyPress={(e) => {
                         e.preventDefault()
                         if (e.key === ' ' || e.key === "Enter") {
-                            this.props.onCharSelected(item.id);
-                            this.focusOnItem(i);
+                            props.onCharSelected(item.id);
+                            focusOnItem(i);
                         }
                     }}>
                     <img src={item.thumbnail} alt={item.name}
@@ -113,34 +102,31 @@ class CharList extends Component {
         )
     }
 
-    render() {
 
-        const { charList, loading, error, newItemLoading, offset, charEnded } = this.state
-        const items = this.view(charList)
+    const items = view(charList);
 
-        const errorMassage = error ? <ErrorMassage /> : null;
-        const spinner = loading ? <Spinner /> : null;
-        const content = !(loading || error) ? items : null;
+    const errorMassage = error ? <ErrorMassage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
 
 
+    return (
+        <div className="char__list" >
 
-        return (
-            <div className="char__list" >
 
+            {errorMassage}
+            {spinner}
+            {content}
 
-                {errorMassage}
-                {spinner}
-                {content}
+            <button className="button button__main button__long"
+                disabled={newItemLoading}
+                onClick={() => onRequest(offset)}
+                style={{ 'display': charEnded ? 'none' : 'block' }}>
+                <div className="inner">load more</div>
+            </button>
+        </div>
+    )
 
-                <button className="button button__main button__long"
-                    disabled={newItemLoading}
-                    onClick={() => this.onRequest(offset)}
-                    style={{ 'display': charEnded ? 'none' : 'block' }}>
-                    <div className="inner">load more</div>
-                </button>
-            </div>
-        )
-    }
 
 }
 
