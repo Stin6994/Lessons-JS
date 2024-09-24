7881d1cb1dd152553e9ce8dd17d53836ca945658
Stin9469 <Stinger1994@mail.ru>
2024-09-24 15:29:16 +0300

    Урок 167 - переписывание проекта на хуки. Переписан CharInfo.js

--- React project (Marvel)/my-app/src/components/charInfo/CharInfo.js
+++ React project (Marvel)/my-app/src/components/charInfo/CharInfo.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import PropTypes from 'prop-types';
 
@@ -12,80 +12,65 @@ import './charInfo.scss';
 
 
 
-class CharInfo extends Component {
-
-    state = {
-        char: null,
-        loading: false,
-        error: false
-    }
-
-    marvelService = new MarvelService();
-
-    componentDidMount() {
-        this.updateChar();
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (this.props.charId !== prevProps.charId) { // ВАЖНО!!! Обязательно использовать prev аргументы, без них будет бесконечная перерисовка и запросы на сервер
-            this.updateChar();
-        }
-    }
+const CharInfo = (props) => {
 
 
-    updateChar = () => {
-        const { charId } = this.props
+    const [char, setChar] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+
+    const marvelService = new MarvelService();
+
+    useEffect(() => {
+        updateChar();
+    }, [props.charId])
+
+    const updateChar = () => {
+        const { charId } = props
         if (!charId) { // если элемент не выбран, используется скелетон
             return;
         }
 
-        this.onCharLoading();
+        onCharLoading();
 
-        this.marvelService
+        marvelService
             .getCharacter(charId)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
+            .then(onCharLoaded)
+            .catch(onError);
     }
 
 
-    onError = () => {
-        this.setState({
-            loading: false,
-            error: true
-        })
-    }
-
-    onCharLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false
-        })
-    }
-
-    onCharLoading = () => {
-        this.setState({
-            loading: true
-        })
+    const onError = () => {
+        setLoading(false);
+        setError(true);
     }
 
 
-    render() {
-        const { char, loading, error } = this.state
-
-        const skeleton = char || loading || error ? null : <Skeleton />
-        const errorMessage = error ? <ErrorMassage /> : null;
-        const spinner = loading ? <Spinner /> : null;
-        const content = !(loading || error || !char) ? <View char={char} /> : null;
-
-        return (
-            <div className="char__info">
-                {skeleton}
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        )
+    const onCharLoaded = (char) => {
+        setChar(char);
+        setLoading(false);
     }
+
+    const onCharLoading = () => {
+        setLoading(true);
+    }
+
+
+    const skeleton = char || loading || error ? null : <Skeleton />
+    const errorMessage = error ? <ErrorMassage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
+
+    return (
+        <div className="char__info">
+            {skeleton}
+            {errorMessage}
+            {spinner}
+            {content}
+        </div>
+    )
+
 
 }
 
@@ -98,7 +83,7 @@ const View = ({ char }) => {
     return (
         <>
             <div className="char__basics">
-                <img src={thumbnail} alt={name} style={thumbnail === imgNotFound ? { objectFit: 'contain' } : { objectFit: 'cover' }}/>
+                <img src={thumbnail} alt={name} style={thumbnail === imgNotFound ? { objectFit: 'contain' } : { objectFit: 'cover' }} />
                 <div>
                     <div className="char__info-name">{name}</div>
                     <div className="char__btns">
