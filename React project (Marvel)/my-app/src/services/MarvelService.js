d70636e1576b1210fde63e09f97ade2a31a0ea43
Stin6994 <Stinger1994@mail.ru>
2024-10-01 21:26:02 +0300

    Урок 174 - навигация готова

--- React project (Marvel)/my-app/src/services/MarvelService.js
+++ React project (Marvel)/my-app/src/services/MarvelService.js
@@ -25,6 +25,18 @@ const useMarvelService = () => {
         // возвращаем из функции переработанный объект только с нужными данными, извлеченными из общей кучи данных
     }
 
+    const getAllComics = async (offset = 0) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`
+		);
+		return res.data.results.map(_transformComics);
+	};
+
+	const getComic = async (id) => {
+		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+		return _transformComics(res.data.results[0]);
+	};
+
     const _transformCharacter = (char) => {
         const thumbnailPath = char.thumbnail.path + '.' + char.thumbnail.extension;
         return {
@@ -40,7 +52,23 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, clearError}
+    const _transformComics = (comics) => {
+		return {
+			id: comics.id,
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
+		};
+	};
+
+    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic}
 }
 
 export default useMarvelService;
