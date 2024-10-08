36633385e4b7f251ef1bf31bd12a9e7a4fb4b691
Stin9469 <Stinger1994@mail.ru>
2024-10-08 16:37:29 +0300

    Урок 181 - Transition group - при открытии окна пропадает кнопка и наоборот

--- React lesson (test)/my-app/src/App.js
+++ React lesson (test)/my-app/src/App.js
@@ -710,7 +710,11 @@ const Modal = (props) => {
 
 
   return (
-    <Transition in={props.show} timeout={duration} >
+    <Transition 
+    in={props.show} 
+    timeout={duration}
+    onEnter={() => props.setShowTrigger(false)}
+    onExited={() => props.setShowTrigger(true)}>
       {state => (
         <div className="modal mt-5 d-block" 
         style={{
@@ -741,14 +745,20 @@ const Modal = (props) => {
 
 function MyModal() {
   const [showModal, setShowModal] = useState(false);
+  const [showTrigger, setShowTrigger] = useState(true);
 
   return (
     <Container>
-      <Modal show={showModal} onClose={setShowModal} />
+      <Modal show={showModal} onClose={setShowModal} setShowTrigger={setShowTrigger}/>
+      {showTrigger ? 
+      
       <button
         type="button"
         className="btn btn-warning mt-5"
-        onClick={() => setShowModal(true)}>Open Modal</button>
+        onClick={() => setShowModal(true)}>Open Modal</button> :
+
+        null}
+
     </Container>
   );
 }
