3523b0ccad88782a001109fd01fd74a7abedebc7
Stin9469 <Stinger1994@mail.ru>
2024-10-29 17:05:02 +0300

    Урок 218 - Bonus - выполнены пункты ТЗ - 3, 4 (формы, данные на локальный сервер)

--- Local server for bonus project/script.js
+++ Local server for bonus project/script.js
@@ -14,7 +14,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const forms = () => {
   const form = document.querySelectorAll('form');
-  const input = document.querySelectorAll('input');
+  const inputs = document.querySelectorAll('input');
+  const phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+  phoneInputs.forEach(item => {
+    item.addEventListener('input', () => {
+      item.value = item.value.replace(/\D/, '');
+    });
+  });
   const message = {
     loading: 'Загрузка...',
     success: 'Спасибо! Скоро мы с вами свяжемся',
@@ -29,7 +35,7 @@ const forms = () => {
     return await res.text();
   };
   const clearInputs = () => {
-    clearInputs.forEach(item => {
+    inputs.forEach(item => {
       item.value = '';
     });
   };
